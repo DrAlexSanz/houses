363fd3cba160d70baff1835c9b1c475e5bbc9ca5
DrAlexSanz <a.sanz.ull@gmail.com>
2024-09-08 19:45:52 +0200

    Add the first 3 files for tomorrow. Structure is in my paper.

---
+++ houses.py
@@ -0,0 +1,11 @@
+# As usual, this is the main file and the functions are in houses_utils.py
+
+import requests
+from bs4 import BeautifulSoup as bs
+import datetime
+
+# Import the URLs from the urls file
+
+from houses_urls import URL_rent_geneva_immobilier
+
+print("Finished script")
